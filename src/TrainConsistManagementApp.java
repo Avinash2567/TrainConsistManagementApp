62cce97e5870ff852db103afe09edcdf73075eff
Avinash k <ak4800@srmist.edu.in>
2026-03-18 09:41:02 +0530

    UC1:Initialize Train and Display Consist Summary

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,17 @@
+import java.util.*;
+
 public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+
+        // Display welcome banner
+        System.out.println("==============================================");
+        System.out.println(" === Train Consist Management App === ");
+        System.out.println("==============================================\n");
+
+        // Create a dynamic list to store train bogies
+        List<String> trainConsist = new ArrayList<>();
+
+        // Display initial consist information
+        System.out.println("Train initialized successfully...");
+    }
 }
